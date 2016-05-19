28972346e7b4852eb8a9aa2d3e4150f9fc021f28
Kazumasa Kohtaka <kkohtaka@gmail.com>
2016-05-19 19:36:25 +0900

    Add a script to reset database

--- models/database.js
+++ models/database.js
@@ -11,23 +11,80 @@
       process.env.DATABASE_PORT,
       process.env.DATABASE_NAME);
 
-  var scheme = {
-    id: 'SERIAL PRIMARY KEY',
-    text: 'VARCHAR(140) not null',
-    created: 'TIMESTAMP',
-    updated: 'TIMESTAMP',
-  };
-  var schemeString = Object.keys(scheme).map(function (key, index) {
-    return key + ' ' + scheme[key];
-  }).join(', ');
+  var createMessagesTable = function (done) {
+    var scheme = {
+      id: 'SERIAL PRIMARY KEY',
+      text: 'VARCHAR(140) NOT NULL',
+      created: 'TIMESTAMP',
+      updated: 'TIMESTAMP',
+    };
+    var schemeString = Object.keys(scheme).map(function (key, index) {
+      return key + ' ' + scheme[key];
+    }).join(', ');
 
-  var client = new pg.Client(connectionString);
-  client.connect();
-  var sql = util.format('CREATE TABLE IF NOT EXISTS messages(%s)', schemeString);
-  var query = client.query(sql);
-  console.log('Creating table...');
-  query.on('end', function () {
-    console.log('Done.');
-    client.end();
-  });
+    var client = new pg.Client(connectionString);
+    client.connect();
+    var sql = util.format('CREATE TABLE IF NOT EXISTS messages(%s)', schemeString);
+
+    console.log('Creating table...');
+    var query = client.query(sql);
+
+    query.on('error', function (err) {
+      console.error('Failed to create.', err);
+      client.end();
+      if (done) done(err);
+    });
+
+    query.on('end', function () {
+      console.log('Created.');
+      client.end();
+      if (done) done(null);
+    });
+  };
+
+  var deleteMessagesTable = function (done) {
+    var client = new pg.Client(connectionString);
+    client.connect();
+    var sql = util.format('DROP TABLE IF EXISTS messages');
+
+    console.log('Dropping table...');
+    var query = client.query(sql);
+
+    query.on('error', function (err) {
+      console.error('Failed to drop.', err);
+      client.end();
+      if (done) done(err);
+    });
+
+    query.on('end', function () {
+      console.log('Dropped.');
+      client.end();
+      if (done) done();
+    });
+  };
+
+  var resetMessagesTable = function (done) {
+    console.log('Resetting table...');
+    deleteMessagesTable(function (err) {
+      if (err) {
+        console.log('Failed to reset.');
+        if (done) done(err);
+      } else {
+        createMessagesTable(function (err) {
+          if (err) {
+            console.log('Failed to reset.');
+          } else {
+            console.log('Reset.');
+          }
+
+          if (done) done(err);
+        });
+      }
+    });
+  };
+
+  module.exports = {};
+  module.exports.createMessagesTable = createMessagesTable;
+  module.exports.deleteMessagesTable = deleteMessagesTable;
+  module.exports.resetMessagesTable = resetMessagesTable;
 }());
