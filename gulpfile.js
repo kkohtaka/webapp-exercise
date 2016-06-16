5942e07ff84161b6aaffe378a7ece9bb6e9342e9
Kazumasa Kohtaka <kkohtaka@gmail.com>
2016-06-16 15:13:31 +0900

    Add docker-compose YAML for testing

--- gulpfile.js
+++ gulpfile.js
@@ -6,40 +6,13 @@
   var gulp = require('gulp');
   var nodemon = require('gulp-nodemon');
   var mocha = require('gulp-mocha');
-  var env = require('gulp-env');
-  var uglify = require('gulp-uglify');
-  var sourcemaps = require('gulp-sourcemaps');
   var livereload = require('gulp-livereload');
   var notify = require('gulp-notify');
   var exit = require('gulp-exit');
   var browserify = require('browserify');
   var babelify = require('babelify');
   var source = require('vinyl-source-stream');
-  var buffer = require('vinyl-buffer');
 
-  var environments = {
-    test: {
-      NODE_ENV: 'test',
-      PORT: 3001,
-      DATABASE_USER: 'postgres',
-      DATABASE_PASS: '',
-      POSTGRES_MASTER_SERVICE_HOST: 'localhost',
-      POSTGRES_MASTER_SERVICE_PORT: '5432',
-      DATABASE_NAME: 'test',
-    },
-    development: {
-      NODE_ENV: 'development',
-      PORT: 3000,
-      DATABASE_USER: 'postgres',
-      DATABASE_PASS: '',
-      POSTGRES_MASTER_SERVICE_HOST: 'localhost',
-      POSTGRES_MASTER_SERVICE_PORT: '5433',
-      DATABASE_NAME: 'development',
-      GOOGLE_CONSUMER_KEY: '206888294905-qvs2m4oudqq7k5cpac4vc6lf1racaffh.apps.googleusercontent.com',
-      GOOGLE_CONSUMER_SECRET: 'avGpzlPDF-vcLhhl4AB4c68w',
-      GOOGLE_OAUTH_CALLBACK_URL: 'http://localhost:3000/auth/google/callback'
-    }
-  }
   gulp.task('serve', function () {
     livereload.listen();
     nodemon({
@@ -51,7 +24,6 @@
         './node_modules/**',
         './client/**'
       ],
-      env: environments.development,
       stdout: true,
     }).on('restart', function () {
       gulp.src('./bin/www')
@@ -81,9 +53,6 @@
   });
 
   gulp.task('mocha', function () {
-    env({
-      vars: environments.test,
-    });
     return gulp.src('./test/**/*.js')
     .pipe(mocha({
       bail: false,
@@ -92,9 +61,6 @@
   });
 
   gulp.task('test', function () {
-    env({
-      vars: environments.test,
-    });
     return gulp.src('./test/**/*.js')
     .pipe(mocha({
       bail: false,
@@ -117,12 +83,6 @@
       this.emit('end');
     })
     .pipe(source('bundle.js'))
-    // .pipe(buffer())
-    // .pipe(sourcemaps.init({
-    //   loadMaps: true,
-    // }))
-    // .pipe(uglify())
-    // .pipe(sourcemaps.write())
     .pipe(gulp.dest('./public/javascripts'));
   });
 
